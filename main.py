ce8ab5d97ecf712e16f2e6abcd07db34220780ba
Пётр <johtai@yandex.ru>
2021-07-27 19:16:39 +0300

    second commit

--- main.py
+++ main.py
@@ -2,23 +2,26 @@ from telegram.ext import Updater
 from telegram.ext import CommandHandler
 import logging
 
-TELEGRAM_TOKEN = open("/res/token.txt", "r")
 
-#use_context - useful arg, =True needs for legacy code and old versions of the lib
-updater = Updater(token=TELEGRAM_TOKEN, use_context=False)
-
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+TELEGRAM_TOKEN = open("res/token.txt").read()
+"logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)"
 
 
 def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Привет, меня зовут Кукко. Я - многофункциональный бот. Буду рад помочь")
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Привет, меня зовут Кукко. Я - многофункциональный бот. Буду рад помочь", \
+        reply_markup=["/commands", "/help"])
 
+
+def help(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Бог в помощь")
+    
 
 def main():
     updater = Updater(TELEGRAM_TOKEN, use_context=True)
     dp = updater.dispatcher
 
     dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help))
 
     updater.start_polling()
     print("Kukko is running")
