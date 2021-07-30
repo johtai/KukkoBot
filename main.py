755b4d93d53d2d38a89858730dce93b6e8f88010
Пётр <johtai@yandex.ru>
2021-07-30 14:31:27 +0300

    safemode initial 2

--- main.py
+++ main.py
@@ -8,22 +8,12 @@ TELEGRAM_TOKEN = open("res/token.txt").read()
 "logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)"
 
 
-with open('res/insults.txt', 'r', encoding="utf-8") as f:
-    insult = (f.read()).split("\n")
-
-
-with open('res/kind.txt', 'r', encoding="utf-8") as f:
-    kind = (f.read()).split("\n")
-
-
 def main():
     updater = Updater(TELEGRAM_TOKEN, use_context=True)
     dp = updater.dispatcher
 
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("help", help))
-    dp.add_handler(CommandHandler("insult", insult_f))
-    dp.add_handler(CommandHandler("wholesome", wholesome))
     dp.add_handler(MessageHandler(Filters.text, response))
 
     updater.start_polling()
@@ -32,16 +22,17 @@ def main():
 
 
 def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Привет, меня зовут Кукко. Я - многофункциональный бот. Буду рад помочь.", \
-        reply_markup=InlineKeyboardMarkup([["/help"]], resize_keyboard=True))
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Привет, меня зовут Кукко. Я - многофункциональный бот. Буду рад помочь.")
 
 
 def response(update, context):
-    print(context.chat_data)
+    print(update.message)
+    print("123")
+    print(update.message["chat"]["type"])
 
 
 def help(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="safemode")
+    context.bot.send_message(chat_id=update.effective_chat.id, text="safemode is running")
 
 
 if __name__ == '__main__':
