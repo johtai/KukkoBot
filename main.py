9d761a933a31bd3459780de7ff5edbba1e9adfa6
johtai <johtai@yandex.ru>
2021-08-07 13:23:00 +0300

    some fixes

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
 from telegram.ext import Updater, Filters
-from telegram.ext import CommandHandler, MessageHandler
-from telegram import ReplyKeyboardMarkup, InlineKeyboardMarkup
+from telegram.ext import CommandHandler, MessageHandler, CallbackQueryHandler
+from telegram import ReplyKeyboardMarkup, InlineKeyboardMarkup, KeyboardButton
 import logging, random
 
 
+#Получение токена и текста со случайными ответами
 TELEGRAM_TOKEN = open("res/token.txt").read()
-
-
 with open('res/response.txt', 'r', encoding="utf-8") as f:
     random_text  = (f.read()).split("\n")
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO)
+
+
+def start(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Привет, меня зовут Кукко. Я — многофункциональный бот. Буду рад помочь")
+
+
+def response(update, context):
+    if update.message["chat"]["type"] != "private":
+        if random.random() < 0.15:
+            update.message.reply_text(random.choice(random_text))
+
+
+def help(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="В обычном режиме я иногда отвечаю на ваши сообщения")
 
 
 def main():
@@ -22,20 +38,6 @@ def main():
     updater.start_polling()
     print("Kukko is running")
     updater.idle()
-
-
-def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Привет, меня зовут Кукко. Я - многофункциональный бот. Буду рад помочь.")
-
-
-def response(update, context):
-    if update.message["chat"]["type"] != "private":
-        if random.random() < 0.11:
-            update.message.reply_text(random.choice(random_text))
-
-
-def help(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="В обычном режиме я иногда отвечаю на ваши сообщения")
 
 
 if __name__ == '__main__':
