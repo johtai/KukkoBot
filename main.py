9a1ff71943132fe1d6f059f0a95c05c18546c90b
johtai <johtai@yandex.ru>
2021-10-10 21:26:23 +0300

    insult, random and kind modes added

--- main.py
+++ main.py
@@ -1,22 +1,37 @@
 from telegram.ext import Updater, Filters
 from telegram.ext import CommandHandler, MessageHandler, CallbackQueryHandler
-from telegram import ReplyKeyboardMarkup, InlineKeyboardMarkup, KeyboardButton
-import logging, random
+from telegram import ReplyKeyboardMarkup, InlineKeyboardMarkup, KeyboardButton, InlineKeyboardButton, ReplyKeyboardRemove
+import logging, random, datetime
 
 
 #Получение токена и текста со случайными ответами
 TELEGRAM_TOKEN = open("res/token.txt").read()
 with open('res/response.txt', 'r', encoding="utf-8") as f:
-    random_text  = (f.read()).split("\n")
+    insult_response  = (f.read()).split("\n")
 
 with open('res/direct_response.txt', 'r', encoding="utf-8") as f:
     direct_response  = (f.read()).split("\n")
+
+with open('res/kind.txt', 'r', encoding="utf-8") as f:
+    kind_response  = (f.read()).split("\n")
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO)
 
 
+def get_mode_keyboard():
+    keyboard = [
+        [
+            InlineKeyboardButton("Добрый", callback_data="kind"),
+            InlineKeyboardButton("Злой", callback_data="insult"),
+            InlineKeyboardButton("Рандом", callback_data="random"),
+        ]
+    ]
+    return InlineKeyboardMarkup(keyboard)
+
+
 def start(update, context):
+    context.chat_data['mode'] = 'insult'
     context.bot.send_message(chat_id=update.effective_chat.id, text="Привет, меня зовут Кукко. Я — многофункциональный бот. Буду рад помочь")
 
 
@@ -25,10 +40,42 @@ def response(update, context):
     #print(msg)
 
     if random.random() < 0.1:
-        if len(msg.split()) == 1:
+        if len(msg.split()) == 1 and context.chat_data['mode'] != 'kind':
             update.message.reply_text(f'{update.message.text} {random.choice(direct_response)}')
         else:
-            update.message.reply_text(random.choice(random_text))
+            if context.chat_data['mode'] == 'kind':
+                kind(update)
+            elif context.chat_data['mode'] == 'insult':
+                insult(update)
+            elif context.chat_data['mode'] == 'random' and random.random() < 0.5:
+                kind(update)
+            else:
+                insult(update)
+
+
+
+def keyboard_callback_handler(update, context):
+    "Обработчик клавиатур"
+
+    query = update.callback_query
+    data = query.data
+    now = datetime.datetime.now()
+    chat_id = update.effective_chat.id
+
+    context.chat_data['mode'] = data
+    query.message.edit_text(text=F'Режим сообщений был изменен на "{data}"')
+
+
+def mode(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text='Выберите режим бота', reply_markup=get_mode_keyboard())
+
+
+def insult(update):
+    update.message.reply_text(random.choice(insult_response))
+
+
+def kind(update):
+    update.message.reply_text(random.choice(kind_response))
 
 
 def help(update, context):
@@ -51,6 +98,8 @@ def main():
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("help", help))
     dp.add_handler(CommandHandler("karelia", karelia))
+    dp.add_handler(CommandHandler("mode", mode))
+    dp.add_handler(CallbackQueryHandler(callback=keyboard_callback_handler))
     dp.add_handler(MessageHandler(Filters.text, response))
     dp.add_handler(MessageHandler(Filters.photo, response))
     dp.add_handler(MessageHandler(Filters.sticker, sticker))
